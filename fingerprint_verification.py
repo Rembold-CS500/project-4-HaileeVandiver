fc662af9ca916f6ba17309533913c9ece2e712a4
Hailee Vandiver <haileevandiver@gmail.com>
2024-08-04 18:23:56 -0700

    getting error Traceback (most recent call last):   File "/Users/hvandiver/cs500/project-4-HaileeVandiver/fingerprint_verification.py", line 54, in <module>     data = read_print("./prints/User1_Original.txt")            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/Users/hvandiver/cs500/project-4-HaileeVandiver/fingerprint_verification.py", line 11, in read_print     with open(data) as fh:               ^^^^ NameError: name 'data' is not defined

--- fingerprint_verification.py
+++ fingerprint_verification.py
@@ -7,31 +7,18 @@
 def read_print(filename):
     """Reads the given print and returns a dictionary with all info."""
     # Your turn!
-
-
-
-
-
-
-# You can add your various check functions below
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    fingerprint_data = {}
+    with open(data) as fh:
+        for line in fh: 
+            #grab first line as name, second line as width, third line height, line 9 on as fingerprint
+            name, username = line.strip()
+            fingerprint_data[name] = username
+            width, fingerprint_width = line[1].strip()
+            fingerprint_data[width] = fingerprint_width
+            height, fingerprint_height = line[2].strip()
+            fingerprint_data[height] = fingerprint_height
+    print(fingerprint_data)
+    return fingerprint_data
 
 
 
